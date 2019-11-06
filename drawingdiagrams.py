38422a9a61d3a7664ae89a28e32af0ccf1ff5b47
Adauto Luis <adauto6@netscape.net>
2019-11-06 08:33:52 -0300

    Adicionada primeira versão para construir o mapa mental a partir do arquivo csv de classificação

--- drawingdiagrams.py
+++ drawingdiagrams.py
@@ -1,4 +1,23 @@
-import turtle             
-my_wn = turtle.Screen()
-turtle.circle(50)
-turtle.write("messi fan", font=("Arial", 16, "normal"))
+
+import pandas as pd
+from graphviz import Digraph
+
+df = pd.read_csv("ranking.csv")
+
+df.head()
+
+f = Digraph('finite_state_machine', filename='MapMinddiagram.gv', format='png')
+
+#f.attr('node', shape='doublecircle',fixedsize='true')
+
+#f.attr(rankdir='LR', size='6,6')
+
+
+for index, row in df.head(20).iterrows():
+#    f.attr('node', shape='doublecircle',scale=str(row['Ocorrence']))
+#    f.attr(rankdir='LR', )
+#    f.attr('node', width='1', height='1')#str(row['Ocorrence']))
+    f.attr('node', width=str(int(row['Ocorrence']*20/3154)), height=str(int(row['Ocorrence']*20/3154)))
+    f.node(row['Palavras'])
+
+f.view()
